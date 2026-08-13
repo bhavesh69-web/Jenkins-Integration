7df2897d7a1a3bfe5e86829ab58918622927ecca
bhavesh19 <bhaveshpcacc@gmail.com>
2026-08-13 08:37:22 +0530

    second commit

--- sample.java
+++ sample.java
@@ -1,5 +1,6 @@
 class sample{
     public static void main(String args[]){
         System.out.println("Hello People");
+        System.out.println("This message will confoen the automation");
     }
 }
